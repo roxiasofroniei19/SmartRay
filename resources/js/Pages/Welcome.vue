94bc37707d2907740e56de0312a220eae9f7a6d6
Asofroniei Roxana <asofroniei.roxi@gmail.com>
2021-02-21 18:11:15 +0200

    First Commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <guest-layout>
+    <template #content> </template>
+  </guest-layout>
+</template>
+
+
+<script>
+import GuestLayout from "../Layouts/GuestLayout.vue";
+export default {
+  components: {
+    GuestLayout,
+  },
+};
+</script>
+
+<style scoped>
+</style>
